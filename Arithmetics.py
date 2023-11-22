9d795d0adfb079699ab1d2f161fd80da12bf4605
Toluwani D. Adeoti <137415857+Shiraga12@users.noreply.github.com>
2023-11-21 19:25:34 -0500

    Update Arithmetics.py

--- Arithmetics.py
+++ Arithmetics.py
@@ -36,14 +36,3 @@ import random
 
 def generate_random_number(start, end):
     return random.randint(start, end)
-def is_even(x):
-    return x % 2 == 0
-def is_odd(x):
-    return x % 2 != 0
-def is_prime(x):
-    if x < 2:
-        return False
-    for i in range(2, int(math.sqrt(x)) + 1):
-        if x % i == 0:
-            return False
-    return True
