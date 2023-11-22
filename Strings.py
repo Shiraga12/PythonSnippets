475c773c2b04dd16639d0618073c92c69e49e1a2
Toluwani D. Adeoti <137415857+Shiraga12@users.noreply.github.com>
2023-11-21 19:24:01 -0500

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -4,8 +4,14 @@ def reverse(string):
     return string[::-1]
 def capitalize(string):
     return string.capitalize()
-def count_occurrences(string, substring):
-    return string.count(substring)
+def count_occurrences(string, substrings):
+    count = 0
+    if len(substrings) > 1:
+        for substring in substrings:
+            count += string.count(substring)
+        return count
+    else:
+        return string.count(substrings)
 def is_palindrome(string):
     return string == string[::-1]
 def remove_whitespace(string):
