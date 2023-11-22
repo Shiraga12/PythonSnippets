ef672ac98d98d20a867015dc10a040519ce111fc
Toluwani D. Adeoti <137415857+Shiraga12@users.noreply.github.com>
2023-11-21 19:24:50 -0500

    Add files via upload

---
+++ Boolean.py
@@ -0,0 +1,11 @@
+def is_even(x):
+    return x % 2 == 0
+def is_odd(x):
+    return x % 2 != 0
+def is_prime(x):
+    if x < 2:
+        return False
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+    return True
